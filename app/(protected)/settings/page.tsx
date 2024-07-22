63219dda257aa138c91e2dc7cecdc954f4d708a0
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-23 00:15:25 +0530

    settings page

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,23 @@
+import { auth } from "@/auth";
+import { Button } from "@/components/ui/button";
+import { logout } from "@/actions/logout";
+
+const SettingsPage = async () => {
+  const session = await auth()
+  return (
+    <div>
+      <pre className="p-4">
+        {
+          JSON.stringify(session, null, 2)
+        }
+      </pre>
+      <form action={logout}>
+        <Button type="submit">
+          Sign Out
+        </Button>
+      </form>
+    </div>
+  )
+}
+
+export default SettingsPage;
