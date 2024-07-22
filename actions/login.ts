19cc2a7c430da0d65695d9aed260970e1fd2c29a
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-23 00:15:17 +0530

    update login server action

--- actions/login.ts
+++ actions/login.ts
@@ -1,6 +1,9 @@
 "use server";
 
+import { signIn } from "@/auth";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 import { LoginSchema, ValidateLoginSchema } from "@/schemas";
+import { AuthError } from "next-auth";
 
 export const login = async (values: ValidateLoginSchema) => {
   const validatedFields = LoginSchema.safeParse(values);
@@ -8,5 +11,25 @@ export const login = async (values: ValidateLoginSchema) => {
   if (!validatedFields.success) {
     return { error: "Invalid Fields" }
   }
-  return { success: "Email sent" }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn('credentials', {
+      email,
+      password,
+      redirectTo: DEFAULT_LOGIN_REDIRECT,
+    })
+    return { success: "successful login!" }
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" }
+        default:
+          return { error: "Something went wrong!" }
+      }
+    }
+    throw error;
+  }
 } 
