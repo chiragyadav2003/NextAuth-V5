c523537e7a54ff00d96933413db93178d452c13b
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-22 11:33:04 +0530

    register user in db

--- actions/register.ts
+++ actions/register.ts
@@ -1,12 +1,35 @@
 "use server";
 
+import bcrypt from 'bcrypt';
+import db from "@/lib/db"
 import { RegisterSchema, ValidateRegisterSchema } from "@/schemas";
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: ValidateRegisterSchema) => {
   const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
-    return { error: "Invalid Fields" }
+    return { error: "Invalid Fields!" }
   }
-  return { success: "Email sent" }
+
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use!" }
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  })
+
+  //TODO: send verification token to email
+
+  return { success: "User created!" }
 } 
