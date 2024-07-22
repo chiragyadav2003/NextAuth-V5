7e17038e1fdb9b40a27bec3bfd573363c26ace06
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-23 00:14:53 +0530

    chore: Refactor middleware to handle authentication and routing logic based on user session

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,51 @@
 import authConfig from "@/auth.config";
-import NextAuth from "next-auth";
+import NextAuth, { Session } from "next-auth";
+
+import {
+  publicRoutes,
+  authRoutes,
+  apiAuthPrefix,
+  DEFAULT_LOGIN_REDIRECT
+} from "@/routes"
+import { NextRequest } from "next/server";
 
 const { auth } = NextAuth(authConfig);
 
-export default auth(async function middleware(req) {
-  const isLoggedIn = !!req.auth;
-  console.log("ROUTE :", req.nextUrl.pathname)
-  console.log("is logged in ", isLoggedIn)
+export default auth((req: NextRequest & { auth: Session | null }): Response | void => {
+  // const isLoggedIn = !!req.auth;
+  // console.log("ROUTE :", req.nextUrl.pathname)
+  // console.log("URL :", req.nextUrl)
+  // console.log("is logged in ", isLoggedIn)
+
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth; //convert to boolean
+
+  //always allow this route, it is required for next auth to work properly
+  // e.g - api/auth/providers
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    //if user is logged inn, redirect him
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL('/auth/login', nextUrl));
+  }
+
+  return;
 })
 
 
