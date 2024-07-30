9c68453ff06e2aa81b6e14d815e15c60f9d70bad
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-07-31 00:12:03 +0530

    add new routes and their pages, fix navbar position

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from '@/app/(protected)/_components/navbar';
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className="mx-auto flex h-full w-full max-w-7xl flex-col items-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800 pt-[20%]">
       <Navbar />
       {children}
     </div>
