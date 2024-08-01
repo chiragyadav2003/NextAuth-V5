1c300ac7961a99495179255ef4342a3e26fe9c16
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-08-01 20:02:35 +0530

    admin page

--- app/(protected)/server/page.tsx
+++ app/(protected)/server/page.tsx
@@ -1,5 +1,6 @@
 import { UserInfo } from '@/components/user-info';
-import { currentUser } from '@/lib/currentUser-auth';
+import { currentUser } from '@/lib/auth';
+
 const ServerPage = async () => {
   const user = await currentUser();
   return (
