e2578c1dcb2c9360dc2e805c0897445b1f975fbe
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-08-02 23:26:22 +0530

    update settings schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,3 +1,5 @@
+import { UserRole } from '@prisma/client';
+import path from 'path';
 import * as z from 'zod';
 
 export const LoginSchema = z.object({
@@ -34,9 +36,52 @@ export const NewPasswordSchema = z.object({
   }),
 });
 
-export const SettingsSchema = z.object({
-  name: z.optional(z.string()),
-});
+export const SettingsSchema = z
+  .object({
+    name: z.optional(z.string()),
+    isTwoFactorEnabled: z.optional(z.boolean()),
+    role: z.enum([UserRole.ADMIN, UserRole.USER]),
+    email: z.optional(z.string().email()),
+    password: z.optional(z.string()),
+    newPassword: z.optional(z.string().min(6)),
+    confirmNewPassword: z.optional(z.string().min(6)),
+  })
+  .refine(
+    (data) => {
+      if (data.password && !data.newPassword) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'Enter new password',
+      path: ['newPassword'],
+    },
+  )
+  .refine(
+    (data) => {
+      if (!data.password && data.newPassword) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'Enter current password',
+      path: ['password'],
+    },
+  )
+  .refine(
+    (data) => {
+      if (data.newPassword !== data.confirmNewPassword) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: 'New passwords do not match',
+      path: ['confirmNewPassword'],
+    },
+  );
 
 export type ValidateLoginSchema = z.infer<typeof LoginSchema>;
 
