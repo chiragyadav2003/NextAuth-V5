a89ecdecc20f4379a211f5c9606283412d4d6ec5
chiragyadav2003 <97416399+chiragyadav2003@users.noreply.github.com>
2024-08-02 23:25:57 +0530

    update layout

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -2,13 +2,11 @@ import { Navbar } from '@/app/(protected)/_components/navbar';
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="mx-auto flex h-full w-full flex-col items-center justify-center gap-y-10 bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
-      <div className="absolute top-20 mx-auto w-full max-w-7xl">
+    <div className="mx-auto flex min-h-screen max-w-screen-2xl flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <div className="mx-auto mt-10 w-full">
         <Navbar />
       </div>
-      <div className="absolute top-80 mx-auto flex w-[60%] max-w-7xl items-center justify-center">
-        {children}
-      </div>
+      <div className="mb-10 mt-10 w-[60%] flex-grow items-center justify-center">{children}</div>
     </div>
   );
 };
